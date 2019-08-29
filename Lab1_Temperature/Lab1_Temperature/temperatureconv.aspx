8717f0c21088760c1198570f19cc755d96f2c492
sunny722122 <52668821+sunny722122@users.noreply.github.com>
2019-08-29 13:03:36 -0600

    Add files via upload

---
+++ Lab1_Temperature/Lab1_Temperature/temperatureconv.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="temperatureconv.aspx.cs" Inherits="Lab1_Temperature.temperatureconv" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+            height: 226px;
+        }
+        .auto-style2 {
+            width: 218px;
+        }
+        .auto-style3 {
+            width: 496px;
+            height: 356px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style3">
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="Larger" Text="Temperature Converter"></asp:Label>
+            <br />
+            <br />
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2" rowspan="5">
+                        <asp:Image ID="Image1" runat="server" ImageUrl="~/img/temp.png" Width="175px" />
+                    </td>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="From"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:Label ID="Label6" runat="server" Text="To"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:DropDownList ID="drdlsttempFrom" runat="server">
+                            <asp:ListItem Value="C">Celsius</asp:ListItem>
+                            <asp:ListItem Value="F">Fahrenheit</asp:ListItem>
+                            <asp:ListItem Value="K">Kelvin</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="drdlsttempTo" runat="server">
+                            <asp:ListItem Value="C">Celsius</asp:ListItem>
+                            <asp:ListItem Value="F">Fahrenheit</asp:ListItem>
+                            <asp:ListItem Value="K">Kelvin</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label4" runat="server" Text="Input temperature:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txttempIn" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label5" runat="server" Text="Output temperature:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txttempOut" runat="server" ReadOnly="True"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="btnConv" runat="server" Text="Convert" OnClick="btnConv_Click" />
+                    </td>
+                    <td>
+                        <asp:Button ID="btnClr" runat="server" Text="Clear" OnClick="btnClr_Click" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lab1_Temperature/Lab1_Temperature/temperatureconv.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab1_Temperature
+{
+    public partial class temperatureconv : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConv_Click(object sender, EventArgs e)
+        {
+            if(txttempIn.Text!="")
+            {
+                double dtempin = Convert.ToDouble(txttempIn.Text);
+                double dtempout = 0;
+                switch (drdlsttempFrom.SelectedItem.Text)
+                {
+                    case "Celsius":
+                        switch(drdlsttempTo.SelectedValue)
+                        {
+                            case "C":
+                                dtempout = dtempin;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                            case "F":
+                               dtempout = (dtempin*9/5)+32;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                            case "K":
+                                dtempout = dtempin +273.15;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                            default:
+                                dtempout = dtempin;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                        }
+                        break;
+                    case "Fahrenheit":
+                        switch (drdlsttempTo.SelectedValue)
+                        {
+                            case "C":
+                                dtempout = (dtempin-32)*5/9;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                            case "F":
+                                dtempout = dtempin;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                            case "K":
+                                dtempout = (dtempin - 32) * 5 / 9 + 273.15;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                            default:
+                                dtempout = dtempin;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                        }
+                        break;
+                    case "Kelvin":
+                        switch (drdlsttempTo.SelectedValue)
+                        {
+                            case "C":
+                                dtempout = dtempin - 273.15;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                            case "F":
+                                dtempout = (dtempin-273.15)*9/5+32;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                            case "K":
+                                dtempout = dtempin;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                            default:
+                                dtempout = dtempin;
+                                txttempOut.Text = Convert.ToString(dtempout);
+                                break;
+                        }
+                        break;
+                    default:
+                        txttempOut.Text = "";
+                        break;
+                }
+            }
+        }
+
+        protected void btnClr_Click(object sender, EventArgs e)
+        {
+            txttempIn.Text = "";
+            txttempOut.Text = "";
+            drdlsttempFrom.SelectedValue = "C";
+            drdlsttempTo.SelectedValue = "C";
+
+        }
+    }
+}
